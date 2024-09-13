37d2d74d499f9ff834f7fcf743092edcb9a1d099
saifmohammad00 <saifmohammad592001@gmail.com>
2024-09-13 19:35:52 +0530

    Saving the Todo status

--- pages/today/completedtasks.js
+++ pages/today/completedtasks.js
@@ -1,7 +1,30 @@
+import TodoList from "../../components/todo/TodoList";
+import { MongoClient } from "mongodb";
 
-function CompletedTasks(){
+function CompletedTasks(props){
     return <div>
         <h1>Completed Tasks</h1>
+        <TodoList todos={props.todos}/>
     </div>
 }
+export async function getStaticProps() {
+    const client = await MongoClient.connect(
+      "mongodb+srv://saifmohammad:Mongo1234@cluster0.9nfvk.mongodb.net/todos?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    const db = client.db();
+    const todoscollection = db.collection("todos");
+    const todos = await todoscollection.find().toArray();
+    client.close();
+    return {
+      props: {
+        todos: todos.filter(task=>task.completed===false)
+        .map(task=>({
+          text:task.text,
+          completed:task.completed,
+          id:task._id.toString(),
+        })),
+      },
+      revalidate: 1,
+    };
+  }
 export default CompletedTasks;
